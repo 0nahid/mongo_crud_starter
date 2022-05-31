4ec3bab1bd8660774dc396cab185d5d1b1223c46
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-31 12:44:58 +0600

    added post api added user add function on backend & intregate with ui added alert for success & error

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5500;
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json());
 require("dotenv").config();
 
 // connect to monogoDB
@@ -18,24 +23,26 @@ async function run() {
     const database = client.db("wifiUsers");
     const usersCollection = database.collection("users");
     // create a document to insert
-    const doc = {
-      firstName: "John",
-      lastName: "Doe",
-      age: 50,
-      interests: ["sports", "movies", "travel"],
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    };
-    const result = await usersCollection.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    // const doc = {
+    //   firstName: "John",
+    //   lastName: "Doe",
+    //   age: 50,
+    //   interests: ["sports", "movies", "travel"],
+    //   content: "No bytes, no problem. Just insert a document, in MongoDB",
+    // };
+    // const result = await usersCollection.insertOne(doc);
+    // console.log(`A document was inserted with the _id: ${result.insertedId}`);
 
     // post api
-    app.post("g", (req, res) => {
+    app.post("/api/users", async (req, res) => {
       const user = req.body;
-      usersCollection.insertOne(user);
-      res.send(user);
+      const result = await usersCollection.insertOne(user);
+      // console.log('got new user', user);
+      // console.log('added user', result);
+      res.json(result);
     });
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
--- frontend/src/components/AddUser/AddUser.js
+++ frontend/src/components/AddUser/AddUser.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import { useRef } from "react";
 
 const AddUser = () => {
-    return (
-        <div>
-            <h2>This is Add User</h2>
-        </div>
-    );
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const handleAddUser = (e) => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { email, name };
+    fetch("http://localhost:5500/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // show alert after successful insert
+        data.insertedId
+          ? alert("User added successfully")
+          : alert("User not added");
+        e.target.reset();
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Please add an user here</h2>
+      <form onSubmit={handleAddUser}>
+        Name : <input type="text" required ref={nameRef} name="firstName" />
+        <br />
+        <br />
+        Email : <input type="email" required ref={emailRef} name="email" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
