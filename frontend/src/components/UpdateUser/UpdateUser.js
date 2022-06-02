e2a3ef1e9053cf402ee834d6765c1e8504fffb02
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-02 13:35:35 +0600

    showed user on ui

--- frontend/src/components/UpdateUser/UpdateUser.js
+++ frontend/src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2>This is Update User</h2>
-        </div>
-    );
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5500/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, [id]);
+
+  return (
+    <div>
+      <div style={{background:'violate'}}>
+        <p>User name : {user.name}</p>
+        <p>User email : {user.email}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateUser;
